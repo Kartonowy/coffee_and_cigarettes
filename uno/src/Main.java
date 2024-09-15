a9b6d9cb8bdc5cebf297250559d97260e5ce35db
Kartonowy <ttrollface.com.pl@gmail.com>
2024-09-15 22:53:13 +0200

    4.3

--- uno/src/Main.java
+++ uno/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -10,21 +11,42 @@ public class Main {
             Scanner r = new Scanner(new File("./liczby.txt"));
             FileWriter fw = new FileWriter("./wyniki4.txt");
             int n_of_pow3 = 0;
+            ArrayList<Integer> nums = new ArrayList<Integer>(500);
+            ArrayList<Integer> greatest = new ArrayList<Integer>();
             while (r.hasNextLine()) {
                 try {
                     int number = Integer.parseInt(r.nextLine());
-                    System.out.println(number);
                     // 4.1
                     if (isPowerofThree(number)) {
                         n_of_pow3++;
                     }
                     // 4.2
                     if (isEqualToFactorial(number)) {
-                        System.out.println("is equal to factorial: " + number);
+                        fw.write("4.2: " + number + "\n");
                     }
+                    // 4.3
+                    if (nums.size() == 1 || nums.isEmpty()) {
+                        nums.add(number);
+                    } else {
+                        int current = gcd_array(nums);
+                        if(gcd(gcd_array(nums), number) == current && current > 1) {
+                            nums.add(number);
+                        } else {
+                            if (greatest.size() < nums.size()) {
+                                greatest = new ArrayList(nums.subList(0, nums.size()));
+                            }
+                            var last = nums.getLast();
+                            nums.clear();
+                            nums.add(last);
+                            nums.add(number);
+                        }
+                    }
+//                    System.out.println(gcd_array(new int[]{72, 144, 216}));
                 } catch ( NumberFormatException e ) {}
             }
-            System.out.println("n:" + n_of_pow3);
+            fw.write("4.1: " +n_of_pow3 + "\n");
+            fw.write("4.3: pierwsza liczba ciągu " + greatest.getFirst() + ", długość " + greatest.size() + ", największy wspólny dzielnik " + gcd_array(greatest) + "\n");
+            fw.close();
         } catch (IOException e) {
             System.out.println("An error occured: \n");
             e.printStackTrace();
@@ -61,6 +83,23 @@ public class Main {
             return n;
         }
     }
+    public static int gcd(int n, int m) {
+        while (m != 0) {
+            int t = n;
+            n = m;
+            m = t % n;
+        }
+        return n;
+    }
+    public static int gcd_array(ArrayList<Integer> arr) {
+        if (arr.size() == 1) {
+            return arr.getFirst();
 
+        }
+        if (arr.size() >= 2) {
+            return gcd(arr.getFirst(), gcd_array(new ArrayList<>(arr.subList(1, arr.size()))));
+        }
+        return 0;
+    }
 }
 
